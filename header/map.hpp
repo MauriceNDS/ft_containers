59788d3573d739becd5490fc53436f83ea022105
MauriceNDS <ds.arsen25@gmail.com>
2022-04-06 15:15:45 +0200

    Save

--- header/map.hpp
+++ header/map.hpp
@@ -11,12 +11,14 @@ namespace ft {
 	struct tree_node {
 
 		T			value;
+		bool		black;
+		bool		isLeft;
 		tree_node*	parent;
 		tree_node*	left;
 		tree_node*	right;
 
-		tree_node( void ) : parent( NULL ), left( NULL ), right( NULL ) {}
-		tree_node( T val ) : value( val ), parent( NULL ), left( NULL ), right( NULL ) {}
+		tree_node( void ) : parent( NULL ), left( NULL ), right( NULL ), black( true ), isLeft( false ) {}
+		tree_node( T val ) : value( val ), parent( NULL ), left( NULL ), right( NULL ), black( false ), isLeft( false ) {}
 
 	};
 
@@ -66,15 +68,177 @@ namespace ft {
 			typedef allocator_type::const_reference								const_reference;
 			typedef allocator_type::pointer										pointer;
 			typedef allocator_type::const_pointer								const_pointer;
-			typedef tree_iterator< pair<key_type, mapped_type> >				iterator;
-			typedef const_tree_iterator< pair<key_type, mapped_type> >			const_iterator;
+			typedef tree_iterator< pair< key_type, mapped_type > >				iterator;
+			typedef const_tree_iterator< pair< key_type, mapped_type > >		const_iterator;
 			typedef reverse_iterator<iterator>									reverse_iterator;
 			typedef const_reverse_iterator<const_iterator>						const_reverse_iterator;
 			typedef std::ptrdiff_t												difference_type;
 			typedef size_t														size_type;
 
+		private:
+
+			tree_node< pair< key_type, mapped_type > > * _root;
+			size_type _size;
+
+			void add( pair< key_type, mapped_type > p ) {
+				tree_node< pair< key_type, mapped_type > > * node = new tree_node< pair< key_type, mapped_type > >( p );
+				if ( root == NULL ) {
+					_root = node;
+					_root.black = true;
+					_size++;
+					return ;
+				}
+				add( _root, node );
+				check_color( node );
+				_size++;
+			}
+
+			void add( tree_node< pair< key_type, mapped_type > > * parent, tree_node< pair< key_type, mapped_type > > * new_node ) {
+				if ( key_compare( parent->key, new_node->key ) ) {
+					if ( parent->right == NULL ) {
+						parent->right = new_node;
+						new_node->parent = parent;
+						new_node->isLeft = false;
+						return ;
+					}
+					return add( parent->right, new_node );
+				}
+				if ( parent->left == NULL ) {
+					parent->left = new_node;
+					new_node->parent = parent;
+					new_node->isLeft = true;
+					return ;
+				}
+				return add( parent->left, new_node );
+			}
+			
+			void check_color( tree_node< pair< key_type, mapped_type > > * node ) {
+				if ( node == root )
+					return ;
+				if ( !node->black && !node->parent->black )
+					correct_tree( node );
+				check_color( node->parent ); 
+			}
+
+			void correct_tree( tree_node< pair< key_type, mapped_type > > * node ) {
+				if ( node->parent->isLeft ) { // aunt is node->parent->parent->right
+					if ( node->parent->parent->right == NULL || node->parent->parent->right->black )
+						return rotate( node );
+					if ( node->parent->parent->right != NULL )
+						node->parent->parent->right->black = true;
+					node->parent->parent->black = false;
+					node->parent->black = true;
+					return ;
+				}
+				// aunt is node->parent->parent->left
+				if ( node->parent->parent->left == NULL || node->parent->parent->left->black )
+					return rotate( node );
+				if ( node->parent->parent->left != NULL )
+					node->parent->parent->left->black = true;
+				node->parent->parent->black = false;
+				node->parent->black = true;
+			}
+
+			void rotate( tree_node< pair< key_type, mapped_type > > * node ) {
+				if ( node->isLeft ) {
+					if ( node->parent->isLeft ) {
+						right_rotate( node->parent->parent );
+						node->black = false;
+						node->parent->black = true;
+						if ( node->parent->right != NULL )
+							node->parent->right->black = false;
+						return ;
+					}
+					rightleft_rotate( node->parent->parent );
+					node->black = true;
+					node->right->black = false;
+					node->left->black = false;
+					return ;
+				}
+				if ( node->parent->isLeft ) {
+					left_rotate( node->parent->parent );
+					node->black = false;
+					node->parent->black = true;
+					if ( node->parent->right != NULL )
+						node->parent->right->black = false;
+					return ;
+				}
+				leftright_rotate( node->parent->parent );
+				node->black = true;
+				node->right->black = false;
+				node->left->black = false;
+			}
+
+			void left_rotate( tree_node< pair< key_type, mapped_type > > * node ) {
+				tree_node< pair< key_type, mapped_type > > * temp = node->right;
+				node->right = temp->left;
+				if ( node->right != NULL ) {
+					node->right->parent = node;
+					node->right->isLeft = false;
+				}
+				if ( node->parent == NULL ) { // we are a root node
+					_root = temp;
+					temp->parent = NULL;
+				}
+				else {
+					temp->parent = node->parent;
+					if ( node->isLeft ) {
+						temp->isLeft = true;
+						temp->parent->left = temp;
+					}
+					else {
+						temp->isLeft = false;
+						temp->parent->right = temp
+					}
+				}
+				temp->left = node;
+				node->isLeft = true;
+				node->parent = temp;
+			}
+
+			void right_rotate( tree_node< pair< key_type, mapped_type > > * node ) {
+				tree_node< pair< key_type, mapped_type > > * temp = node->left;
+				node->left = temp->right;
+				if ( node->left != NULL ) {
+					node->left->parent = node;
+					node->left->isLeft = true;
+				}
+				if ( node->parent == NULL ) { // we are a root node
+					_root = temp;
+					temp->parent = NULL;
+				}
+				else {
+					temp->parent = node->parent;
+					if ( node->isLeft ) {
+						temp->isLeft = true;
+						temp->parent->left = temp;
+					}
+					else {
+						temp->isLeft = false;
+						temp->parent->right = temp
+					}
+				}
+				temp->right = node;
+				node->isLeft = false;
+				node->parent = temp;
+			}
+
+			void leftright_rotate( tree_node< pair< key_type, mapped_type > > * node ) {
+				left_rotate( node->left );
+				right_rotate( node );
+			}
+
+			void rightleft_rotate( tree_node< pair< key_type, mapped_type > > * node ) {
+				left_rotate( node->left );
+				right_rotate( node );
+			}
+
+		public:
+
 			class value_comp {
+
 				friend class map;
+
 			protected:
 
 				Compare comp;
@@ -90,7 +254,6 @@ namespace ft {
 				}
 
 			};
-
 
 	};
 
